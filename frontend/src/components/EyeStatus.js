555d233872b11f41109fdb430cc4f9e64b812eb4
Jimmy Yang <jimmyy1337@gmail.com>
2021-05-08 12:18:37 -0400

    bootstrap

---
+++ frontend/src/components/EyeStatus.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function EyeStatus() {
+  return (
+    <div>
+      <h1> Eye Status: </h1>
+      <p> very bad </p>
+    </div>
+  )
+}
---
+++ frontend/src/layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import EyeStatus from '../components/EyeStatus'
+
+export default function Layout() {
+  return (
+    <div>
+      <EyeStatus />
+    </div>
+  )
+}
