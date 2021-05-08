d438c740b5e82a6df2d05b7a622283cbf9566741
Jimmy Yang <jimmyy1337@gmail.com>
2021-05-08 12:29:59 -0400

    bs with basic layout

--- frontend/src/components/EyeStatus.js
+++ frontend/src/components/EyeStatus.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 export default function EyeStatus() {
   return (
     <div>
       <h1> Eye Status: </h1>
       <p> very bad </p>
+      <Button> See a doctor </Button>
     </div>
   )
 }
--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import EyeStatus from '../components/EyeStatus'
 
+import styles from './Layout.module.css'
+import { Container } from 'react-bootstrap'
+
 export default function Layout() {
   return (
-    <div>
-      <EyeStatus />
-    </div>
+    <Container >
+      <div className="d-flex flex-row">
+        <EyeStatus />
+        <EyeStatus />
+        <EyeStatus />
+
+      </div>
+    </Container>
   )
 }
