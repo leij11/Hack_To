00d696643d923cabc9dc3b7c9296b78aab8c6d66
RhysFernandes <rhysnfernandes@gmail.com>
2021-05-08 23:58:29 -0400

    Sync up flask and opencv

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from multiprocessing import Process, Value
+from opencv import Blink
 
 # Server default is 127.0.0.1:5000
 app = Flask(__name__)
 CORS(app)
+
+blink = Blink()
 
 def make_response(res, error_code):
     res = jsonify(res)
@@ -23,6 +27,17 @@ def index():
 def get_status():
     res = {
         'eye_status': 'good',
-        'bpm': 30,
+        'bpm': blink.get_bpm(),
     }
     return make_response({'res': res}, 200)
+
+#def record_loop():
+#    blink.run()
+#    print('proc')
+
+if __name__ == "__main__":
+    #p = Process(target=blink.run())
+    #p.start()
+    app.run(host='127.0.0.1',port='5000', debug=False, use_reloader=False)
+    #p.join()
+    blink.run()
