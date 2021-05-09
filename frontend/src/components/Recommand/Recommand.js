fcd1c46c4de0d15558683764aceb7b55001d7181
Jimmy Yang <jimmyy1337@gmail.com>
2021-05-09 10:30:33 -0400

    recommends working

--- frontend/src/components/Recommand/Recommand.js
+++ frontend/src/components/Recommand/Recommand.js
@@ -1,5 +1,5 @@
-import React, { useState,useEffect } from "react";
-import { CardDeck,Card} from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { CardDeck, Card } from 'react-bootstrap';
 import Panel from '../../layout/Panel/Panel';
 import blurry from './blurry.jpeg';
 import redeye from './redeye.jpeg';
@@ -23,89 +23,90 @@ export default function Recommand() {
             {supported_Symptoms.map(Symptom => <option key={Symptom}>{Symptom}</option>)}
         </select>
     )
-  return (
-    <>
-    <Panel>
-        <h1> Recommandation</h1>
-        <CardDeck style={{marginTop: "30px"}}>
-            { 
-            <Card>
-                <Card.Body>
-                <img src={redeye} width="200" height="200"/>
-                <Card.Title>Red Eye</Card.Title>
-                <input style={{marginRight: "20px"}} type="checkbox" />
-                <label> Use Eye Drop</label>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"></small>
-                </Card.Footer>
-            </Card>
-            }
-            { 
-            <Card>
-                <Card.Body>
-                <Card.Title>Tearing</Card.Title>
-                <img src={tearing} width="200" height="200"/>
-                <div className="d-flex flex-column justify-content-center align-items-left">
-                    <label> 
-                        <input style={{marginRight: "20px"}} type="checkbox" /> 
+    return (
+        <>
+            <Panel>
+                <h1> Recommandation</h1>
+                <SymptomDropdown />
+                <CardDeck style={{ marginTop: "30px" }}>
+                    {Symptom == 'Red Eye' &&
+                        <Card>
+                            <Card.Body>
+                                <img src={redeye} width="200" height="200" />
+                                <Card.Title>Red Eye</Card.Title>
+                                <input style={{ marginRight: "20px" }} type="checkbox" />
+                                <label> Use Eye Drop</label>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"></small>
+                            </Card.Footer>
+                        </Card>
+                    }
+                    {Symptom == 'Tearing' &&
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Tearing</Card.Title>
+                                <img src={tearing} width="200" height="200" />
+                                <div className="d-flex flex-column justify-content-center align-items-left">
+                                    <label>
+                                        <input style={{ marginRight: "20px" }} type="checkbox" />
                         Close your eyes for a minute
                     </label>
-                    <label>
-                        <input style={{marginRight: "20px"}} type="checkbox" />
-                        Blink Quickly for each seconds 
+                                    <label>
+                                        <input style={{ marginRight: "20px" }} type="checkbox" />
+                        Blink Quickly for each seconds
                     </label>
-                    <label>
-                        <input style={{marginRight: "20px"}} type="checkbox" />
+                                    <label>
+                                        <input style={{ marginRight: "20px" }} type="checkbox" />
                         Use Eye Drop
                     </label>
-                </div>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"></small>
-                </Card.Footer>
-            </Card>
-            }
-             { 
-            <Card>
-                <Card.Body>
-                <Card.Title>Burning</Card.Title>
-                <img src={burning} width="200" height="100"/>
-                <div className="d-flex flex-column justify-content-center align-items-left">
-                    <label> 
-                        <input style={{marginRight: "20px"}} type="checkbox" /> 
+                                </div>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"></small>
+                            </Card.Footer>
+                        </Card>
+                    }
+                    {Symptom == 'Burning' &&
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Burning</Card.Title>
+                                <img src={burning} width="200" height="100" />
+                                <div className="d-flex flex-column justify-content-center align-items-left">
+                                    <label>
+                                        <input style={{ marginRight: "20px" }} type="checkbox" />
                         Rinse eyes with warm water
                     </label>
-                    <label>
-                        <input style={{marginRight: "20px"}} type="checkbox" />
+                                    <label>
+                                        <input style={{ marginRight: "20px" }} type="checkbox" />
                         Take a break from screen for 5 minutes
                     </label>
-                </div>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"></small>
-                </Card.Footer>
-            </Card>
-            }
-            { 
-            <Card>
-                <Card.Body>
-                <Card.Title>Blurry</Card.Title>
-                <img src={blurry} width="300" height="200"/>
-                <div style={{marginTop: "20px"}} className="d-flex flex-column justify-content-center align-items-left">
-                    <label>
-                        <input style={{marginRight: "20px"}} type="checkbox" />
+                                </div>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"></small>
+                            </Card.Footer>
+                        </Card>
+                    }
+                    {Symptom == 'Blurry' &&
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Blurry</Card.Title>
+                                <img src={blurry} width="300" height="200" />
+                                <div style={{ marginTop: "20px" }} className="d-flex flex-column justify-content-center align-items-left">
+                                    <label>
+                                        <input style={{ marginRight: "20px" }} type="checkbox" />
                         Take a break from screen for 5 minutes
                     </label>
-                </div>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted"></small>
-                </Card.Footer>
-            </Card>
-            }
-            </CardDeck>
+                                </div>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted"></small>
+                            </Card.Footer>
+                        </Card>
+                    }
+                </CardDeck>
             </Panel>
-    </>
-  )
+        </>
+    )
 }
