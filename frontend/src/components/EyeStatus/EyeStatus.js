a50bfc143054e445e51eded38e5ceb065d99fd85
leij11 <leij11@hotmail.com>
2021-05-08 20:12:29 -0400

    Connect backend to frontend

--- frontend/src/components/EyeStatus/EyeStatus.js
+++ frontend/src/components/EyeStatus/EyeStatus.js
@@ -1,15 +1,41 @@
-import React from 'react'
-import Panel from '../../layout/Panel/Panel'
+import React, { useState,useEffect } from "react";
+import Panel from '../../layout/Panel/Panel';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
-export default function EyeStatus({ eyeStatusShow, eyeStatus }) {
+export default function EyeStatus({ eyeStatusShow }) {
+  const [eyeInfo,setEyeInfo]= useState([])
+
+  useEffect(() => {
+    const getInfo = () => {
+        let source=axios.CancelToken.source();
+        axios
+        .get(`http://127.0.0.1:5000/get_status`, {
+            cancelToken: source.token,
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then(response => {
+            const eyeInfo = response.data.res
+            setEyeInfo(eyeInfo);
+        })
+        .catch(error => {
+            if (!axios.isCancel(error)) {
+                setEyeInfo();
+            }
+            console.log(error)
+        })
+    
+      }
+    getInfo();
+  }, []);
+
   return (
     <>
       { eyeStatusShow &&
         <Panel>
           <h1> Eye Status: </h1>
           <span aria-label="Very Bad" role="img"> 😩</span>
-          <p> { eyeStatus } </p>
+          <p> { eyeInfo['eye_status'] } </p>
           <Button> See a doctor </Button>
         </Panel>
       }
